816b407c00de96fdbd813ecce95e989e63608a27
AzureSoar <lixiangyu0404@163.com>
2020-08-06 19:37:26 +0800

    Update vue.config.js
    assetsConfig

--- zero_width_char_encoding/vue.config.js
+++ zero_width_char_encoding/vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
       ? '/zero-width-char-encoding/zero_width_char_encoding/'
-      : '/'
+      : '/',
+    assetsDir:./
+    
   }
