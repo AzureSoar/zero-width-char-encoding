32ea5c8bf18fa3fc241270bb9118d4c770b3d31e
li_xiangyu3 <lixiangyu0404@163.com>
2020-08-07 09:42:11 +0800

    7th9.41

--- zero_width_char_encoding/vue.config.js
+++ zero_width_char_encoding/vue.config.js
@@ -2,6 +2,6 @@ module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
       ? '/zero-width-char-encoding/zero_width_char_encoding/'
       : '/',
-    assetsDir:'./dist/'
+    // assetsDir:'./dist/'
     
   }
